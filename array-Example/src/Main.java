323a09b91205fc79b20b730598c49d3c87fa8b91
Rutuja <rutujakamble7147@gmail.com>
2023-01-19 10:15:20 +0530

    adding array-Example module

---
+++ array-Example/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    int[]oddNumber = {1,3,5,7,9,11};
+        System.out.println("third element of array is index " +oddNumber[2]);
+        oddNumber[4] = 21;
+        for (int i:oddNumber
+             ) {
+            System.out.println(i);
+        }
+    }
+
+}
