ef20de68a74b5d82827d06ac071e9840a046c573
Rutuja <rutujakamble7147@gmail.com>
2023-01-29 20:25:26 +0530

    Adding Collection-sort-vs-arrys-sort-example module

---
+++ Collections-sort-vs-arrys-sort-example/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> guestList = new ArrayList<>();
+        guestList.add("rutuja");
+        guestList.add("anjali");
+        guestList.add("sakshi");
+        guestList.add("kiran");
+
+        Collections.sort(guestList);
+
+        Integer[] evenNumber = {2,4,6,8,10};
+
+        Arrays.sort(evenNumber);
+    }
+}
